60f641f78e30d52fae362955c1a683e79e4f44cd
Jan-Marcus Sivadi <jmarcus@hot.ee>
2020-11-26 14:17:02 +0200

    commit 1

---
+++ resources/views/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test (ON/OFF)</title>
+</head>
+
+<body>
+
+    <div class="container" style="width: 35%; height: 50vh; margin: 0 auto;">
+        <img id="img" src="img/1.png" style="width: 100%;">
+        <button id="btn">Click Me</button>
+    </div>
+    
+    <script src="js/app.js"></script>
+</body>
+
+</html>
