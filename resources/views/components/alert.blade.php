1aa28640f823959a693bec0476d61bff436d4fa9
Jan-Marcus Sivadi <jmarcus@hot.ee>
2020-12-29 23:50:40 +0200

    Todo List Mini Project 6 | Show All Todo

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -6,4 +6,12 @@
         {{$slot}}
         <div class="alert alert-danger">{{session()->get('error')}}</div>
     @endif
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                {{ $error }}
+            @endforeach
+        </div>
+    @endif
 </div>
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,1 +1,13 @@
-All todos
+@extends('todos.layout')
+
+@section('content')
+        <h1 class="d-flex justify-content-center page-title">All Your Todos</h1>
+        <hr class="hr-1">
+        <ul class="list-group">
+            @foreach($todos as $todo)
+            <li class="d-flex justify-content-center list-group-item">
+                {{$todo->title}}
+            </li>
+            @endforeach
+        </ul>
+@endsection
