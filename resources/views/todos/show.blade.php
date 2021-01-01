9e0f756866bdafd56e4248bc8faf9a1382dac30d
Jan-Marcus Sivadi <jmarcus@hot.ee>
2021-01-02 00:55:48 +0200

    Todo List Mini Project 29 | Show each steps

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -7,6 +7,19 @@
     <div class="d-flex justify-content-center">
         <div id="show-description">{{$todo->description}}</div>
     </div>
+
+    @if($todo->steps->count() > 0)
+        <h2 class="d-flex justify-content-center" style="padding-top: 1rem">Steps for this task</h2>
+        <hr class="hr-1">
+
+        @foreach($todo->steps as $step)
+            <li class="list-group-item d-flex justify-content-center align-items-center" id="vertical-align-item">
+                <a>
+                    {{$step->name}}
+                </a>
+            </li>
+        @endforeach
+    @endif
         
     <div class="d-flex justify-content-center">
         <a class="btn btn-primary" id="Back-btn" href="{{route('todo.index')}}" role="button">Back to List</a>
